6aafac4866fc461f96d39f09e31b23db9169419c
fpsven <95655446+fpsven@users.noreply.github.com>
2023-10-03 22:30:43 +0200

    Add files via upload

---
+++ Dtos/Account/AddAccountDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Dtos.Character
+{
+    public class AddAccountDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } =  "test";
+
+        //en meer dingfen toevoegen die bij accounts horen(gegevens uit database en andere files zoals GetAccountDto en UpdateCharacterDto)
+    }
+}
---
+++ models/Account.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.models
+{
+    public class Account
+    {
+        
+    }
+}
