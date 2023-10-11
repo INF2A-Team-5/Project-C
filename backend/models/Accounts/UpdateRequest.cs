f99c80d815ef0476362f7bb0feee89a6e9b58b69
Jeffrey <Jeffmanvan@gmail.com>
2023-10-12 00:06:30 +0200

    commented some unnescesary files

--- backend/models/Accounts/UpdateRequest.cs
+++ backend/models/Accounts/UpdateRequest.cs
@@ -1,18 +1,18 @@
-namespace backend.Accounts;
+// namespace backend.Accounts;
 
-using System.ComponentModel.DataAnnotations;
-using backend.Entities;
+// using System.ComponentModel.DataAnnotations;
+// using backend.Entities;
 
-public class UpdateRequest
-{
-    public long Id {get; set; }
-    public string Name { get; set; }
-    public string Password { get; set; }
-    public AccountType Class { get; set; }
+// public class UpdateRequest
+// {
+//     public long Id {get; set; }
+//     public string Name { get; set; }
+//     public string Password { get; set; }
+//     public AccountType Class { get; set; }
 
 
-    private string replaceEmptyWithNull(string value)
-    {
-        return string.IsNullOrEmpty(value) ? null : value;
-    }
-}
+//     private string replaceEmptyWithNull(string value)
+//     {
+//         return string.IsNullOrEmpty(value) ? null : value;
+//     }
+// }
