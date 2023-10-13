bf3b62b2f66872961f53556d733032089e86cf6b
marieclairex <1052281@hr.nl>
2023-10-13 15:11:08 +0200

    gekloot met pop up

--- frontend/src/components/pages/tickets.tsx
+++ frontend/src/components/pages/tickets.tsx
@@ -1,13 +1,26 @@
 import Button from '../foundations/button'
-import React from 'react';
+import Header from '../foundations/header'
+import Input from '../foundations/input'
+import React, { ComponentProps } from 'react';
+import Badge from'../foundations/badge'
 
-function Tickets() {
-  return (
-    <div>
-      <h1>Tickets</h1>
-      <Button>Refresh</Button>
-    </div>
-  )
+function myFunction() {
+  const popup = document.getElementById("myPopup");
+
+  if (popup) {
+    popup.classList.toggle("show");
+  }
 }
 
+
+function Tickets() {
+
+  return (
+  <div>
+    <Button hierarchy='xl' intent="primary" onClick={myFunction} rounded="slight">Pop up<span className="popuptext" id="myPopup">Popup text...</span></Button>
+    
+  </div>
+  )
+}
 export default Tickets
+//export default Tickets
