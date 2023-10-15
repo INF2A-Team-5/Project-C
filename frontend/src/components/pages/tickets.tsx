44b46e034136d6a2d7521f2dd3ada8610f209ca1
marieclairex <1052281@hr.nl>
2023-10-15 14:36:43 +0200

    poging 4?

--- frontend/src/components/pages/tickets.tsx
+++ frontend/src/components/pages/tickets.tsx
@@ -17,31 +17,30 @@ function myFunction() {
 function Tickets() {
 
   return (
-    
-  <><div className='top'>
-    {/* <Header></Header> */}
-    <label><h1>Report error</h1></label>
-    </div>
-  <div className='left'>
-      {/* <Button hierarchy='xl' intent="primary" onClick={myFunction} rounded="slight">Pop up<span className="popuptext" id="myPopup">Popup text...</span></Button> */}
+    <><div className='flex-container'>
+      {/* <div><Header></Header></div> */}
+      <div><label><h1>Report error</h1></label></div>
+    </div><div className="flex-container">
+        <div></div>
+        <div><div>
+          {/* <Button hierarchy='xl' intent="primary" onClick={myFunction} rounded="slight">Pop up<span className="popuptext" id="myPopup">Popup text...</span></Button> */}
 
-      {/* <Block size = "xl" color = "gray"> <label><h2>What do you see?</h2></label></Block> */}
-      <h2>What do you see?</h2>
-      <Input hierarchy='xxl'></Input>
-      <h2>What should it do?</h2>
-      <Input hierarchy='xxl'></Input>
-    </div>
-    
-    <div className="mid">
-      <h2>What have you tried?</h2>
-      <Input hierarchy='xxl'></Input>
-      <h2>Enter phone number</h2>
-      <Input hierarchy='xxl'></Input>
-      </div>
-      
-      <div className="right">
-      <h2>Upload videos/pictures</h2>
-      <Input hierarchy='xxl'></Input>  
+          {/* <Block size = "xl" color = "gray"> <label><h2>What do you see?</h2></label></Block> */}
+          <h2>What do you see?</h2>
+          <Input hierarchy='xxl'></Input>
+          <h2>What should it do?</h2>
+          <Input hierarchy='xxl'></Input>
+        </div></div>
+        <div><div>
+          <h2>What have you tried?</h2>
+          <Input hierarchy='xxl'></Input>
+          <h2>Enter phone number</h2>
+          <Input hierarchy='xxl'></Input>
+        </div></div>
+        <div>      <div>
+          <h2>Upload videos/pictures</h2>
+          <Input hierarchy='xxl'></Input>
+        </div></div>
       </div></>
   )
 }
