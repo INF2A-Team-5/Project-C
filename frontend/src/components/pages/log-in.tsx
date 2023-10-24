61145c23087efdc13c979cb6db5e3a17f2df068a
fpsven <sven.groeneveld.nl@gmail.com>
2023-10-24 12:19:32 +0200

    .

--- frontend/src/components/pages/log-in.tsx
+++ frontend/src/components/pages/log-in.tsx
@@ -42,7 +42,7 @@ function LogIn() {
   return (
     
     <div className='content grid-container'>
-      <div className='grid-item login-left'>
+      <div className='login-left'>
         <div className='wrapper'>
           <h1>Fully designing your process in</h1>
           <div className='words'>
@@ -54,32 +54,29 @@ function LogIn() {
           </div>
         </div>
       </div>
-      <div className='grid-item'>
+      <div className='login-right'>
         <Header></Header>
-        <h2>Login</h2>
-        <h3>Username</h3>
-        <div>
-          
-          {/* <label htmlFor="username"></label> */}
-          <Input hierarchy='xl' name='username' placeholder='Enter Username'
-          onChange={e => setUsername(e.currentTarget.value)}
-          //onChange={e => setUsername(e.target.value)}
-          />
+        <div className='login-form'>
+          <h2>Login</h2>
+          <h3>Username</h3>
+          <div>
+            <Input hierarchy='xl' name='username' placeholder='Client1'
+            onChange={e => setUsername(e.currentTarget.value)}
+            />
+          </div>
+          <h3>Password</h3>
+          <div>
+            <Input hierarchy='xl' name='password' placeholder='******'
+            onChange={e => setPassword(e.currentTarget.value)}
+            />
+          </div>
+          <label>
+            {/* <input type="checkbox" className="checkbox" name="remember"/> Remember me */}
+          </label>
+          <br />
+          <Button hierarchy='xl' intent="primary" onClick={handleSubmit} rounded="slight">Log in</Button>
+          <Button hierarchy='xl' intent="primary" onClick={toggleTheme} rounded="slight">theme</Button>    
         </div>
-        <h3>Password</h3>
-        <div>
-          {/* <label htmlFor="password"></label> */}
-          <Input hierarchy='xl' name='password' placeholder='Enter Password'
-          onChange={e => setPassword(e.currentTarget.value)}
-          />
-        </div>
-        {/* <label>
-          <input type="checkbox" class="defaultcheckbox" name="remember"> Remember me
-        </label> */}
-        <br />
-        <Button hierarchy='xl' intent="primary" onClick={handleSubmit} rounded="slight">Log in</Button>
-        <Button hierarchy='xl' intent="primary" onClick={toggleTheme} rounded="slight">theme</Button>    
-      {/* </form> */}
       </div>
       
     </div>
