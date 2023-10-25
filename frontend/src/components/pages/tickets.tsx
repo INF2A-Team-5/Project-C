15fb9c16986e5d2eabda1d322eb57a91717738a9
marieclairex <1052281@hr.nl>
2023-10-26 00:15:34 +0200

    versie 5 (niet af)

--- frontend/src/components/pages/tickets.tsx
+++ frontend/src/components/pages/tickets.tsx
@@ -4,6 +4,8 @@ import Input from '../foundations/input'
 import React, { ComponentProps } from 'react';
 import Badge from'../foundations/badge'
 import Block from'../foundations/block'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function myFunction() {
   const popup = document.getElementById("myPopup");
@@ -11,9 +13,7 @@ function myFunction() {
   if (popup) {
     popup.classList.toggle("show");
   }
-import React from 'react';
-import AccountService from '../../services/AccountService';
-
+}
 // export interface Machine {
 //   MachineId: number; Name: string; Description: string; AccountId: number
 // }
@@ -23,39 +23,56 @@ import AccountService from '../../services/AccountService';
 // }
 
 function Tickets() {
-  async function ChooseMachine() {
-    const account = await fetch("http://localhost:5119/api/accounts").then((res) => res.json()).then(accounts => accounts.find((acc: any) => acc.accountId == 1))
-    let machines = await fetch("http://localhost:5119/api/machines").then((res) => res.json()).then(machines => machines.filter((machine: any) => machine.accountId == account.accountId))
-    console.log(machines)
+  const [information, setInformation] = useState('');
+  const [phonenumber, setPhonenumber] = useState('');
+  const [pictures, setPictures] = useState('');
+  const navigate = useNavigate();
+  // async function ChooseMachine() {
+  //   const account = await fetch("http://localhost:5119/api/accounts").then((res) => res.json()).then(accounts => accounts.find((acc: any) => acc.accountId == 1))
+  //   let machines = await fetch("http://localhost:5119/api/machines").then((res) => res.json()).then(machines => machines.filter((machine: any) => machine.accountId == account.accountId))
+  //   console.log(machines)
+
+  async function handleSubmit() {
+    if (information != null && phonenumber != null)
+    {
+      alert("Ticket submitted");
+      navigate('/tickets');
+    }
+    else
+    {
+      alert("You haven't filled in all necessary fields");
+    }
   }
+
   return (
     <><div className='flex-container'>
-      <Button onClick={ChooseMachine}>Refresh</Button>
+      {/* <Button onClick={ChooseMachine}>Refresh</Button> */}
       {/* <div><Header></Header></div> */}
       <div><label><h1>Report error</h1></label></div>
     </div><div className="flex-container">
         <div></div>
         <div><div>
-          {/* <Button hierarchy='xl' intent="primary" onClick={myFunction} rounded="slight">Pop up<span className="popuptext" id="myPopup">Popup text...</span></Button> */}
+          <Button hierarchy='xl' intent="primary" onClick={myFunction} rounded="slight">Pop up<span className="popuptext" id="myPopup">Popup text...</span></Button>
 
           {/* <Block size = "xl" color = "gray"> <label><h2>What do you see?</h2></label></Block> */}
-          <h2>What do you see?</h2>
-          <Input hierarchy='xxl'></Input>
+          <h2>What do you see?*</h2>
+          <Input hierarchy='xxl' onChange={e => setInformation(e.currentTarget.value)}/>
           <h2>What should it do?</h2>
           <Input hierarchy='xxl'></Input>
         </div></div>
         <div><div>
-          <h2>What have you tried?</h2>
+          <h2>What have you tried?*</h2>
           <Input hierarchy='xxl'></Input>
           <h2>Enter phone number</h2>
-          <Input hierarchy='xxl'></Input>
+          <Input hierarchy='xxl' onChange={e => setPhonenumber(e.currentTarget.value)}/>
         </div></div>
-        <div>      <div>
+        <div><div>
           <h2>Upload videos/pictures</h2>
-          <Input hierarchy='xxl'></Input>
+          <Input hierarchy='xxl' onChange={e => setPictures(e.currentTarget.value)}/><br></br><br></br>
+          <Button hierarchy='xl' intent="primary" onClick={handleSubmit} rounded="slight">Submit</Button>
         </div></div>
       </div></>
   )
-}
+  }
 export default Tickets
 //export default Tickets
