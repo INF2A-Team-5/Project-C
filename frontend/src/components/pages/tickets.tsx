05ffb460920e66c0702653488946366a3c4131e5
CasGijtenbeek <cas@gijtenbeek.tk>
2023-11-09 17:10:06 +0100

    Tickets made in the tickets page are now pushed to the db

--- frontend/src/components/pages/tickets.tsx
+++ frontend/src/components/pages/tickets.tsx
@@ -2,11 +2,11 @@ import Button from '../foundations/button'
 import Header from '../foundations/header'
 import Input from '../foundations/input'
 import React, { ComponentProps, useEffect } from 'react';
-import Badge from'../foundations/badge'
-import Block from'../foundations/block'
+import Badge from '../foundations/badge'
+import Block from '../foundations/block'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
 import axios from 'axios';
 
 // export interface Machine {
@@ -28,9 +28,9 @@ function Tickets() {
   // Hiermee aan het kutten, maar is nog niet gefixt 
   const upload = (file: File, onUploadProgress: any): Promise<any> => {
     let formData = new FormData();
-  
+
     formData.append("file", file);
-  
+
     return axios.post("/upload", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
@@ -39,7 +39,7 @@ function Tickets() {
     });
   };
 
-  const getFiles = () : Promise<any> => {
+  const getFiles = (): Promise<any> => {
     return axios.get("/files");
   };
 
@@ -52,51 +52,45 @@ function Tickets() {
   }
 
   async function handleSubmit() {
-    if (problem.length != 0 && phonenumber.length != 0 && mustbedoing.length != 0 && havetried.length != 0)
-    {
+    if (problem.length != 0 && phonenumber.length != 0 && mustbedoing.length != 0 && havetried.length != 0) {
       if (problem.length < 100 || mustbedoing.length < 100) {
         alert("The first 2 answers must contain atleast 90 characters")
         navigate('/tickets');
       }
-      else
-      {
-      let information = {"Problem" : problem, "Must be doing": mustbedoing, "Have tried": havetried};
-      var ticket = 
-      {
-        Machine_Id: 1,
-        Customer_Id: 1,
-        Priority: "unknown",
-        Status: "To do",
-        Date_Created: new Date(),
-        Information: information,
-        Solution: "x",
-        Pictures: "x",
-        PhoneNumber: phonenumber,
-        Notes: ""
-      }
+      else {
+        let information = { "Problem": problem, "Must be doing": mustbedoing, "Have tried": havetried };
+        var ticket =
+        {
+          Machine_Id: 1,
+          Customer_Id: 1,
+          Priority: "unknown",
+          Status: "To do",
+          Date_Created: new Date(),
+          Information: information,
+          Solution: "x",
+          Pictures: "x",
+          PhoneNumber: phonenumber,
+          Notes: ""
+        }
 
-      const pushticket = axios.put("http://localhost:5119/api/tickets", [1, ticket])
-      .then(res => 
-          {console.log("Message successfully updated", res);})
-      .catch(err => 
-          {console.log("Message could not be updated", err)});
-      pushticket
-          
-      alert("Ticket submitted");
-      navigate('/tickets');
+        const pushticket = axios.post("http://localhost:5119/api/tickets", ticket)
+          .then(res => { console.log("Message successfully updated", res); })
+          .catch(err => { console.log("Message could not be updated", err) });
+        pushticket
 
-      
+        alert("Ticket submitted");
+        navigate('/tickets');
+
+
       }
     }
-    else
-    {
+    else {
       alert("You haven't filled in all necessary fields");
     }
   }
 
-    
-  async function Popup() 
-  {
+
+  async function Popup() {
     alert("First, we have a few questions (fill in the first block):'\n1. Is the machine turned on?\n2. Does the machine still move(for a part)?")
   }
 
@@ -105,38 +99,38 @@ function Tickets() {
   }, []);
 
   return (
-    
+
     <div>
       {/* <Button onClick={ChooseMachine}>choose machine</Button> */}
       {/* <div><Header></Header></div> */}
       <div><label><h1>Report error</h1></label></div>
-          {/* <Button hierarchy='xl' intent="primary" onClick={myFunction} rounded="slight">Pop up<span className="popuptext" id="myPopup">Popup text...</span></Button> */}
+      {/* <Button hierarchy='xl' intent="primary" onClick={myFunction} rounded="slight">Pop up<span className="popuptext" id="myPopup">Popup text...</span></Button> */}
 
-          {/* <Block size = "xl" color = "gray"> <label><h2>What do you see?</h2></label></Block> */}
-        <h2>What do you see?* (Atleast 20 words)</h2>
-        <Input hierarchy='xxl' onChange={e => setProblem(e.currentTarget.value)}/>
+      {/* <Block size = "xl" color = "gray"> <label><h2>What do you see?</h2></label></Block> */}
+      <h2>What do you see?* (Atleast 20 words)</h2>
+      <Input hierarchy='xxl' onChange={e => setProblem(e.currentTarget.value)} />
 
-        <h2>What should it do?* (Atleast 20 words)</h2>
-        <Input hierarchy='xxl'onChange={e => setMustBeDoing(e.currentTarget.value)}/>
+      <h2>What should it do?* (Atleast 20 words)</h2>
+      <Input hierarchy='xxl' onChange={e => setMustBeDoing(e.currentTarget.value)} />
 
-        <h2>What have you tried?*</h2>
-        <Input hierarchy='xxl'onChange={e => setHaveTried(e.currentTarget.value)}/>
+      <h2>What have you tried?*</h2>
+      <Input hierarchy='xxl' onChange={e => setHaveTried(e.currentTarget.value)} />
 
-        <h2>Enter phone number</h2>
-        <div className="checkbox-wrapper-6">
-        <input className="tgl tgl-light" id="cb1-6" type="checkbox"/>
+      <h2>Enter phone number</h2>
+      <div className="checkbox-wrapper-6">
+        <input className="tgl tgl-light" id="cb1-6" type="checkbox" />
         <label className="tgl-btn" htmlFor="cb1-6"></label><label>Use from account</label>
-        </div>
-        <Input hierarchy='xxl' onChange={e => setPhonenumber(e.currentTarget.value)}/>
-        
-
-        
-
-          <h2>Upload videos/pictures</h2>
-          <Input hierarchy='xxl' onChange={e => setPictures(e.currentTarget.value)}/><br></br><br></br>
-          <Button hierarchy='xl' intent="primary" onClick={handleSubmit} rounded="slight">Submit</Button>
       </div>
+      <Input hierarchy='xxl' onChange={e => setPhonenumber(e.currentTarget.value)} />
+
+
+
+
+      <h2>Upload videos/pictures</h2>
+      <Input hierarchy='xxl' onChange={e => setPictures(e.currentTarget.value)} /><br></br><br></br>
+      <Button hierarchy='xl' intent="primary" onClick={handleSubmit} rounded="slight">Submit</Button>
+    </div>
 
   )
-  }
+}
 export default Tickets
