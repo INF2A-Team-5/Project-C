5143a218f4a026e497b2dc7c68b734374cc6a7ca
Jeffrey <Jeffmanvan@gmail.com>
2023-11-12 11:19:55 +0100

    yes

--- backend/entities/Account.cs
+++ backend/entities/Account.cs
@@ -5,5 +5,5 @@ namespace backend.Entities;
         public int AccountId { get; set; }
         public string Name { get; set; } = null! ;
         public string Password {get; set;} = null !;
-        public AccountType Class { get; set; } = AccountType.Client;
+        public AccountType Class { get; set; }
     }
