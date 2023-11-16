80809da8049ca94ca8e4ce0bed8f2cfe1f3abefb
CasGijtenbeek <cas@gijtenbeek.tk>
2023-11-16 12:31:56 +0100

    fixed table

---
+++ frontend/src/components/foundations/table.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DataTable, { TableColumn } from "react-data-table-component";
+import Card from "@material-ui/core/Card";
+import SortIcon from "@material-ui/icons/ArrowDownward";
+
+// interface type_of_user {
+//     clientID: number;
+// }
+
+type DataRow = {
+    ticketId: number,
+    machine_Id: number,
+    customer_Id: number,
+    assigned_Id: number,
+    priority: string,
+    status: string,
+    date_Created: string,
+    solution: string,
+    pictures: string,
+    phoneNumber: string,
+    notes: string;
+};
+
+const customer_Id = 1
+
+const data = await fetch("http://localhost:5119/api/Tickets").then((res) => res.json());
+// .then(tickets => tickets.filter((client: any) => client.customer_Id == customer_Id));
+
+const columns: TableColumn<DataRow>[] = [
+    {
+        name: "ID",
+        selector: row => row.ticketId,
+        sortable: true
+    },
+    {
+        name: "Priority",
+        selector: row => row.priority,
+        sortable: true
+    },
+    {
+        name: "Client",
+        selector: row => row.customer_Id,
+        sortable: true
+    },
+    {
+        name: "Date",
+        selector: row => row.date_Created,
+        sortable: true
+    },
+    {
+        name: "Status",
+        selector: row => row.status,
+        sortable: true
+    }
+];
+
+function Table() {
+    return (
+        <div>
+            <Card>
+                <DataTable
+                    columns={columns}
+                    data={data}
+                    defaultSortFieldId="name"
+                    sortIcon={<SortIcon />}
+                    pagination
+                    selectableRows
+                />
+            </Card>
+        </div>
+    )
+}
+
+export default Table;
--- frontend/src/components/pages/tickets.tsx
+++ frontend/src/components/pages/tickets.tsx
@@ -2,8 +2,8 @@ import Button from '../foundations/button'
 import Header from '../foundations/header'
 import Input from '../foundations/input'
 import React, { ComponentProps, SetStateAction, useEffect, useRef } from 'react';
-import Badge from'../foundations/badge'
-import Block from'../foundations/block'
+import Badge from '../foundations/badge'
+import Block from '../foundations/block'
 import UploadService from "../../services/FileUploadService";
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -33,17 +33,16 @@ function Tickets() {
   class Machine {
     name: string;
     id: number;
-    constructor(name: string, id: number)
-    {
+    constructor(name: string, id: number) {
       this.name = name;
       this.id = id
     }
-   }
-   var allmachines: Machine[] = [];
-   // async function uploadimages() {
+  }
+  var allmachines: Machine[] = [];
+  // async function uploadimages() {
   //   ImagesUpload
   // }
-  
+
   // const ImagesUpload: React.FC = () => {
   //   const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
   //   const [imagePreviews, setImagePreviews] = useState<Array<string>>([]);
@@ -51,16 +50,16 @@ function Tickets() {
   //   const [message, setMessage] = useState<Array<string>>([]);
   //   const [imageInfos, setImageInfos] = useState<Array<IFile>>([]);
   //   const progressInfosRef = useRef<any>(null);
-    
+
   //   const selectImages = (event: React.ChangeEvent<HTMLInputElement>) => {
   //     let images: Array<string> = [];
   //     let files = event.target.files;
-  
+
   //     if (files) {
   //       for (let i = 0; i < files.length; i++) {
   //         images.push(URL.createObjectURL(files[i]));
   //       }
-    
+
   //       setSelectedFiles(files);
   //       setImagePreviews(images);
   //       setProgressInfos([]);
@@ -70,22 +69,22 @@ function Tickets() {
   //   const uploadImages = () => {
   //     if (selectedFiles != null) {
   //       const files = Array.from(selectedFiles);
-  
+
   //       let _progressInfos = files.map((file) => ({
   //         percentage: 0,
   //         fileName: file.name
   //       }));
-  
+
   //       progressInfosRef.current = _progressInfos;
-  
+
   //       const uploadPromises = files.map((file, i) => upload(i, file));
-  
+
   //       Promise.all(uploadPromises)
   //         .then(() => UploadService.getFiles())
   //         .then((images) => {
   //           setImageInfos(images.data);
   //         });
-  
+
   //       setMessage([]);
   //     }
   //   };
@@ -107,12 +106,12 @@ function Tickets() {
   //       .catch((err: any) => {
   //         _progressInfos[idx].percentage = 0;
   //         setProgressInfos(_progressInfos);
-  
+
   //         let msg = file.name + ": Failed!";
   //         if (err.response && err.response.data && err.response.data.message) {
   //           msg += " " + err.response.data.message;
   //         }
-  
+
   //         setMessage((prevMessage) => [
   //           ...prevMessage,
   //           msg
@@ -147,14 +146,14 @@ function Tickets() {
   //             </div>
   //           </div>
   //         ))}
-  
+
   //       <div className="row my-3">
   //         <div className="col-8">
   //           <label className="btn btn-default p-0">
   //             <input type="file" multiple accept="image/*" onChange={selectImages} />
   //           </label>
   //         </div>
-  
+
   //         <div className="col-4">
   //           <button
   //             className="btn btn-success btn-sm"
@@ -165,7 +164,7 @@ function Tickets() {
   //           </button>
   //         </div>
   //       </div>
-  
+
   //       {imagePreviews && (
   //         <div>
   //           {imagePreviews.map((img, i) => {
@@ -175,7 +174,7 @@ function Tickets() {
   //           })}
   //         </div>
   //       )}
-  
+
   //       {message.length > 0 && (
   //         <div className="alert alert-secondary mt-2" role="alert">
   //           <ul>
@@ -185,7 +184,7 @@ function Tickets() {
   //           </ul>
   //         </div>
   //       )}
-  
+
   //       {imageInfos.length > 0 && (
   //         <div className="card mt-3">
   //           <div className="card-header">List of Images</div>
@@ -207,17 +206,15 @@ function Tickets() {
 
   // Hiermee aan het kutten, maar is nog niet gefixt 
 
-  async function ChooseMachine()
-  {
+  async function ChooseMachine() {
     let currentaccount = await fetch("http://localhost:5119/api/accounts")
-    .then((res) => res.json())
-    .then(accounts => accounts.find((acc: any) => acc.accountId == 3));
+      .then((res) => res.json())
+      .then(accounts => accounts.find((acc: any) => acc.accountId == 3));
 
     let machinelist = await fetch("http://localhost:5119/api/machines")
-    .then((res) => res.json())
-    .then(machines => machines.filter((machine: any) => machine.accountId == currentaccount.accountId));
-    for (var machine of machinelist)
-    {
+      .then((res) => res.json())
+      .then(machines => machines.filter((machine: any) => machine.accountId == currentaccount.accountId));
+    for (var machine of machinelist) {
       allmachines.push(new Machine(machine.name, machine.machineId));
     }
     SetAccount(currentaccount.accountId);
@@ -225,26 +222,23 @@ function Tickets() {
   }
   async function handleSubmit() {
 
-    if (problem.length != 0 && phonenumber.length != 0 && mustbedoing.length != 0 && havetried.length != 0)
-    {
+    if (problem.length != 0 && phonenumber.length != 0 && mustbedoing.length != 0 && havetried.length != 0) {
       if (problem.length < 100 || mustbedoing.length < 100) {
         alert("The first 2 answers must contain atleast 90 characters")
         navigate('/tickets');
       }
-      if (selectMachine == "")
-      {
+      if (selectMachine == "") {
         alert("Please choose a machine");
         navigate('/tickets');
       }
-      else
-      {
-        let information = {"Problem" : problem, "Must be doing": mustbedoing, "Have tried": havetried};
-        var ticket = 
+      else {
+        let information = { "Problem": problem, "Must be doing": mustbedoing, "Have tried": havetried };
+        var ticket =
         {
           Machine_Id: selectMachine.split("Id: ")[1],
           Customer_Id: account,
           Priority: "unknown",
-          Status: "To do",
+          Status: "Open",
           Date_Created: new Date(),
           Information: information,
           Solution: "x",
@@ -254,23 +248,20 @@ function Tickets() {
         }
 
         const pushticket = await axios.post('http://localhost:5119/api/tickets/', ticket)
-        .then(res => 
-            {console.log("Message successfully updated", res);})
-        .catch(err => 
-            {console.log("Message could not be updated", err)});
+          .then(res => { console.log("Message successfully updated", res); })
+          .catch(err => { console.log("Message could not be updated", err) });
         pushticket
         alert("Ticket submitted");
         navigate('/client');
 
       }
     }
-    else
-    {
+    else {
       alert("You haven't filled in all necessary fields");
     }
   }
 
-    
+
   // async function Popup() 
   // {
   //   alert("First, we have a few questions (fill in the first block):'\n1. Is the machine turned on?\n2. Does the machine still move(for a part)?")
@@ -282,96 +273,96 @@ function Tickets() {
   // useEffect(() => {
   //   uploadimages(); // Call the Popup function when the component mounts
   // }, []);
-    const [showDropDown, setShowDropDown] = useState<boolean>(false);
-    const [selectMachine, setSelectMachine] = useState<string>("");
-    const machines = () => {
-      return machinenames;
-    };
-    /**
-     * Toggle the drop down menu
-     */
-    const toggleDropDown = () => {
-      setShowDropDown(!showDropDown);
-      ChooseMachine();
-    };
-    /**
-     * Hide the drop down menu if click occurs
-     * outside of the drop-down element.
-     *
-     * @param event  The mouse event
-     */
-    const dismissHandler = (event: React.FocusEvent<HTMLButtonElement>): void => {
-      if (event.currentTarget === event.target) {
-        setShowDropDown(false);
-      }
-    };
-  
-    /**
-     * Callback function to consume the
-     * machine name from the child component
-     *
-     * @param machine  The selected machine
-     */
-    const machineSelection = (machine: string): void => {
-      setSelectMachine(machine);
-    };
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
+  const [selectMachine, setSelectMachine] = useState<string>("");
+  const machines = () => {
+    return machinenames;
+  };
+  /**
+   * Toggle the drop down menu
+   */
+  const toggleDropDown = () => {
+    setShowDropDown(!showDropDown);
+    ChooseMachine();
+  };
+  /**
+   * Hide the drop down menu if click occurs
+   * outside of the drop-down element.
+   *
+   * @param event  The mouse event
+   */
+  const dismissHandler = (event: React.FocusEvent<HTMLButtonElement>): void => {
+    if (event.currentTarget === event.target) {
+      setShowDropDown(false);
+    }
+  };
+
+  /**
+   * Callback function to consume the
+   * machine name from the child component
+   *
+   * @param machine  The selected machine
+   */
+  const machineSelection = (machine: string): void => {
+    setSelectMachine(machine);
+  };
   return (
-    
+
     <div>
-        <div className="announcement">
-          <div>{"First, we have a few questions (fill in the first block):"}</div>
-          <div>{"1. Is the machine turned on?"}</div>
-          <div>{"2. Does the machine still move(for a part)?"}</div>
-        </div>
-        <button
-          className={showDropDown ? "active" : undefined}
-          onClick={(): void => toggleDropDown()}
-          onBlur={(e: React.FocusEvent<HTMLButtonElement>): void =>
-            dismissHandler(e)
-          }
-        >
-          <div>{selectMachine ? "Select: " + selectMachine : "Select ..."} </div>
-          {showDropDown && (
-            <DropDown
-              machines={machines()}
-              showDropDown={false}
-              toggleDropDown={(): void => toggleDropDown()}
-              machineSelection={machineSelection}
-            />
-          )}
-        </button>
-              {/* <Button onClick={ChooseMachine}>choose machine</Button> */}
+      <div className="announcement">
+        <div>{"First, we have a few questions (fill in the first block):"}</div>
+        <div>{"1. Is the machine turned on?"}</div>
+        <div>{"2. Does the machine still move(for a part)?"}</div>
+      </div>
+      <button
+        className={showDropDown ? "active" : undefined}
+        onClick={(): void => toggleDropDown()}
+        onBlur={(e: React.FocusEvent<HTMLButtonElement>): void =>
+          dismissHandler(e)
+        }
+      >
+        <div>{selectMachine ? "Select: " + selectMachine : "Select ..."} </div>
+        {showDropDown && (
+          <DropDown
+            machines={machines()}
+            showDropDown={false}
+            toggleDropDown={(): void => toggleDropDown()}
+            machineSelection={machineSelection}
+          />
+        )}
+      </button>
+      {/* <Button onClick={ChooseMachine}>choose machine</Button> */}
 
       <div><Header></Header></div>
       ImagesUpload
       <div><label><h1>Report error</h1></label></div>
-          {/* <Button hierarchy='xl' intent="primary" onClick={myFunction} rounded="slight">Pop up<span className="popuptext" id="myPopup">Popup text...</span></Button> */}
+      {/* <Button hierarchy='xl' intent="primary" onClick={myFunction} rounded="slight">Pop up<span className="popuptext" id="myPopup">Popup text...</span></Button> */}
 
-          {/* <Block size = "xl" color = "gray"> <label><h2>What do you see?</h2></label></Block> */}
-        <h2>What do you see?* (Atleast 20 words)</h2>
-        <Input hierarchy='xxl' onChange={e => setProblem(e.currentTarget.value)}/>
+      {/* <Block size = "xl" color = "gray"> <label><h2>What do you see?</h2></label></Block> */}
+      <h2>What do you see?* (Atleast 20 words)</h2>
+      <Input hierarchy='xxl' onChange={e => setProblem(e.currentTarget.value)} />
 
-        <h2>What should it do?* (Atleast 20 words)</h2>
-        <Input hierarchy='xxl'onChange={e => setMustBeDoing(e.currentTarget.value)}/>
+      <h2>What should it do?* (Atleast 20 words)</h2>
+      <Input hierarchy='xxl' onChange={e => setMustBeDoing(e.currentTarget.value)} />
 
-        <h2>What have you tried?*</h2>
-        <Input hierarchy='xxl'onChange={e => setHaveTried(e.currentTarget.value)}/>
+      <h2>What have you tried?*</h2>
+      <Input hierarchy='xxl' onChange={e => setHaveTried(e.currentTarget.value)} />
 
-        <h2>Enter phone number</h2>
-        <div className="checkbox-wrapper-6">
-        <input className="tgl tgl-light" id="cb1-6" type="checkbox"/>
+      <h2>Enter phone number</h2>
+      <div className="checkbox-wrapper-6">
+        <input className="tgl tgl-light" id="cb1-6" type="checkbox" />
         <label className="tgl-btn" htmlFor="cb1-6"></label><label>Use from account</label>
-        </div>
-        <Input hierarchy='xxl' onChange={e => setPhonenumber(e.currentTarget.value)}/>
-      
-          <h2>Upload videos/pictures</h2>
-          <Settings></Settings>
-          <Input hierarchy='xxl' onChange={e => setPictures(e.currentTarget.value)}/><br></br><br></br>
-          <Button hierarchy='xl' intent="primary" onClick={handleSubmit} rounded="slight">Submit</Button>
       </div>
-      
+      <Input hierarchy='xxl' onChange={e => setPhonenumber(e.currentTarget.value)} />
+
+      <h2>Upload videos/pictures</h2>
+      <Settings></Settings>
+      <Input hierarchy='xxl' onChange={e => setPictures(e.currentTarget.value)} /><br></br><br></br>
+      <Button hierarchy='xl' intent="primary" onClick={handleSubmit} rounded="slight">Submit</Button>
+    </div>
+
 
   )
-  }
-  
+}
+
 export default Tickets
