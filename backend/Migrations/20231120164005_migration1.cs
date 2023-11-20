559636d2af3b081f0a2496c3c0b335be66167922
marieclairex <1052281@hr.nl>
2023-11-20 18:50:50 +0100

    fixed photos again

--- backend/Migrations/20231120164005_migration1.cs
+++ backend/Migrations/20231120164005_migration1.cs
@@ -7,7 +7,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace backend.Migrations
 {
     /// <inheritdoc />
-    public partial class migrationName : Migration
+    public partial class migration1 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -38,6 +38,22 @@ namespace backend.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    FileId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Ticket_Id = table.Column<int>(type: "integer", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: true),
+                    FileType = table.Column<string>(type: "text", nullable: true),
+                    FilePath = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.FileId);
                 });
 
             migrationBuilder.CreateTable(
@@ -81,11 +97,14 @@ namespace backend.Migrations
                     Assigned_Id = table.Column<int>(type: "integer", nullable: false),
                     Priority = table.Column<string>(type: "text", nullable: false),
                     Status = table.Column<string>(type: "text", nullable: false),
+                    Problem = table.Column<string>(type: "text", nullable: false),
+                    HaveTried = table.Column<string>(type: "text", nullable: false),
+                    MustBeDoing = table.Column<string>(type: "text", nullable: false),
                     Date_Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    Solution = table.Column<string>(type: "text", nullable: false),
-                    Pictures = table.Column<string>(type: "text", nullable: false),
-                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
-                    Notes = table.Column<string>(type: "text", nullable: false)
+                    Solution = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    Notes = table.Column<string>(type: "text", nullable: true),
+                    Files = table.Column<string[]>(type: "text[]", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -101,6 +120,9 @@ namespace backend.Migrations
 
             migrationBuilder.DropTable(
                 name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Files");
 
             migrationBuilder.DropTable(
                 name: "Machines");
--- frontend/src/components/pages/tickets.tsx
+++ frontend/src/components/pages/tickets.tsx
@@ -43,172 +43,7 @@ function Tickets() {
     }
   }
 
-  // async function uploadimages() {
-  //   ImagesUpload
-  // }
-
-  // const ImagesUpload: React.FC = () => {
-  //   const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
-  //   const [imagePreviews, setImagePreviews] = useState<Array<string>>([]);
-  //   const [progressInfos, setProgressInfos] = useState<Array<ProgressInfo>>([]);
-  //   const [message, setMessage] = useState<Array<string>>([]);
-  //   const [imageInfos, setImageInfos] = useState<Array<IFile>>([]);
-  //   const progressInfosRef = useRef<any>(null);
-
-  //   const selectImages = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //     let images: Array<string> = [];
-  //     let files = event.target.files;
-
-  //     if (files) {
-  //       for (let i = 0; i < files.length; i++) {
-  //         images.push(URL.createObjectURL(files[i]));
-  //       }
-
-  //       setSelectedFiles(files);
-  //       setImagePreviews(images);
-  //       setProgressInfos([]);
-  //       setMessage([]);
-  //     }
-  //   };
-  //   const uploadImages = () => {
-  //     if (selectedFiles != null) {
-  //       const files = Array.from(selectedFiles);
-
-  //       let _progressInfos = files.map((file) => ({
-  //         percentage: 0,
-  //         fileName: file.name
-  //       }));
-
-  //       progressInfosRef.current = _progressInfos;
-
-  //       const uploadPromises = files.map((file, i) => upload(i, file));
-
-  //       Promise.all(uploadPromises)
-  //         .then(() => UploadService.getFiles())
-  //         .then((images) => {
-  //           setImageInfos(images.data);
-  //         });
-
-  //       setMessage([]);
-  //     }
-  //   };
-
-  //   const upload = (idx: number, file: File) => {
-  //     let _progressInfos = [...progressInfosRef.current];
-  //     return UploadService.upload(file, (event: { loaded: number; total: number; }) => {
-  //       _progressInfos[idx].percentage = Math.round(
-  //         (100 * event.loaded) / event.total
-  //       );
-  //       setProgressInfos(_progressInfos);
-  //     })
-  //       .then(() => {
-  //         setMessage((prevMessage) => [
-  //           ...prevMessage,
-  //           file.name + ": Successful!"
-  //         ]);
-  //       })
-  //       .catch((err: any) => {
-  //         _progressInfos[idx].percentage = 0;
-  //         setProgressInfos(_progressInfos);
-
-  //         let msg = file.name + ": Failed!";
-  //         if (err.response && err.response.data && err.response.data.message) {
-  //           msg += " " + err.response.data.message;
-  //         }
-
-  //         setMessage((prevMessage) => [
-  //           ...prevMessage,
-  //           msg
-  //         ]);
-  //       });
-  //   };
-
-  //   useEffect(() => {
-  //     UploadService.getFiles().then((response) => {
-  //       setImageInfos(response.data);
-  //     });
-  //   }, []);
-
-  //   return (
-  //     <div>
-  //       {progressInfos &&
-  //         progressInfos.length > 0 &&
-  //         progressInfos.map((progressInfo: ProgressInfo, index: number) => (
-  //           <div className="mb-2" key={index}>
-  //             <span>{progressInfo.fileName}</span>
-  //             <div className="progress">
-  //               <div
-  //                 className="progress-bar progress-bar-info"
-  //                 role="progressbar"
-  //                 aria-valuenow={progressInfo.percentage}
-  //                 aria-valuemin={0}
-  //                 aria-valuemax={100}
-  //                 style={{ width: progressInfo.percentage + "%" }}
-  //               >
-  //                 {progressInfo.percentage}%
-  //               </div>
-  //             </div>
-  //           </div>
-  //         ))}
-
-  //       <div className="row my-3">
-  //         <div className="col-8">
-  //           <label className="btn btn-default p-0">
-  //             <input type="file" multiple accept="image/*" onChange={selectImages} />
-  //           </label>
-  //         </div>
-
-  //         <div className="col-4">
-  //           <button
-  //             className="btn btn-success btn-sm"
-  //             disabled={!selectedFiles}
-  //             onClick={uploadImages}
-  //           >
-  //             Upload
-  //           </button>
-  //         </div>
-  //       </div>
-
-  //       {imagePreviews && (
-  //         <div>
-  //           {imagePreviews.map((img, i) => {
-  //             return (
-  //               <img className="preview" src={img} alt={"image-" + i} key={i} />
-  //             );
-  //           })}
-  //         </div>
-  //       )}
-
-  //       {message.length > 0 && (
-  //         <div className="alert alert-secondary mt-2" role="alert">
-  //           <ul>
-  //             {message.map((item, i) => {
-  //               return <li key={i}>{item}</li>;
-  //             })}
-  //           </ul>
-  //         </div>
-  //       )}
-
-  //       {imageInfos.length > 0 && (
-  //         <div className="card mt-3">
-  //           <div className="card-header">List of Images</div>
-  //           <ul className="list-group list-group-flush">
-  //             {imageInfos.map((img, index) => (
-  //               <li className="list-group-item" key={index}>
-  //                 <p>
-  //                   <a href={img.url}>{img.name}</a>
-  //                 </p>
-  //                 <img src={img.url} alt={img.name} height="80px" />
-  //               </li>
-  //             ))}
-  //           </ul>
-  //         </div>
-  //       )}
-  //     </div>
-  //   );
-  // }
-
-  // Hiermee aan het kutten, maar is nog niet gefixt 
+  
 
   async function ChooseMachine()
   {
@@ -264,13 +99,6 @@ function Tickets() {
 
   async function handleSubmit() {
 
-    // if (typeof file === "undefined") {
-    //   console.log('File is undefined');
-    //   return;
-    // }
-
-
-
     if (problem.length != 0 && phonenumber.length != 0 && mustbedoing.length != 0 && havetried.length != 0)
     {
       if (problem.split(" ").length < 20 || mustbedoing.split(" ").length < 20) {
@@ -298,7 +126,7 @@ function Tickets() {
           HaveTried: havetried,
 
           Solution: "x",
-          Pictures: preview,
+          files: preview,
           PhoneNumber: phonenumber,
           Notes: ""
         }
