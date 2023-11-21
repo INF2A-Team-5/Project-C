9b361fa8a497182608f9784bea76b4997e544ca5
Sven Groeneveld <95655446+fpsven@users.noreply.github.com>
2023-11-21 11:55:58 +0100

    my butooonn

--- frontend/src/components/pages/client.tsx
+++ frontend/src/components/pages/client.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 import Table from "../foundations/table";
 
 function Client() {
+  function toTicket() {
+    window.location.href = window.location.href.replace("client", "tickets");
+  }
   return (
     <div>
       <h1>Client</h1>
       <Table></Table>
+      <div>
+        <Button hierarchy="md" type="primary" onClick={toTicket}>Yes I have</Button>
+      </div>
     </div>
   );
 }
