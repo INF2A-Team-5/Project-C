31b143c56a235233237ac0e7684196374aef7be9
CasGijtenbeek <cas@gijtenbeek.tk>
2023-11-23 19:43:39 +0100

    Made new table component

---
+++ frontend/src/components/foundations/newTable.tsx
@@ -0,0 +1,41 @@
+
+interface TableProps {
+    data: { [key: string]: any }[];
+    displayColumns: string[];
+    dataColumns: string[];
+}
+
+function NewTable({ data, displayColumns, dataColumns }: TableProps) {
+
+    function handleButtonClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: number) {
+        alert(id)
+    }
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {displayColumns.map((column, index) => (
+                        <th key={index}>{column}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {dataColumns.map((column, columnIndex) => (
+                            <td key={columnIndex}>{row[column]}</td>
+                        ))}
+                        {rowIndex === data.length - 1 && handleButtonClick ? (
+                            <td>
+                                <button onClick={(e) => handleButtonClick(e, row.ticketId)}>Click me</button>
+                            </td>
+                        ) : null}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default NewTable;
--- frontend/src/components/pages/serviceEmployee.tsx
+++ frontend/src/components/pages/serviceEmployee.tsx
@@ -1,13 +1,48 @@
 import Button from "../foundations/button";
-import React from 'react';
+import React, { useState } from 'react';
 import Settings from '../foundations/settings'
+import NewTable from '../foundations/newTable';
 
 function serviceEmployee() {
+  const [Tickets, SetTickets] = useState<DataRow[]>([]);
+  console.log(Tickets);
+  if (Tickets.length == 0) {
+    GetData();
+  }
+
+  type DataRow = {
+    ticketId: number,
+    machine_Id: number,
+    customer_Id: number,
+    assigned_Id: number,
+    priority: string,
+    status: string,
+    date_Created: string,
+    solution: string,
+    files: string[],
+    phoneNumber: string,
+    notes: string;
+  };
+
+  async function GetData() {
+    SetTickets(await fetch("http://localhost:5119/api/tickets/",
+      {
+        method: "GET",
+        headers:
+        {
+          "Authorization": "bearer " + localStorage.getItem("Token"),
+          "Content-Type": "application/json",
+        }
+      })
+      .then(data => data.json())
+      .then(tickets => tickets.filter((client: any) => client.customer_Id == localStorage.getItem("Id"))));
+  }
+
   return (
     <div>
       <Settings></Settings>
       <h1>serviceEmployee</h1>
-      <Button>Refresh</Button>
+      <NewTable displayColumns={["ID", "Priority", "Client", "Date", "Status", ""]} data={Tickets} dataColumns={["ticketId", "priority", "customer_Id", "date_Created", "status"]} />
     </div>
   );
 }
