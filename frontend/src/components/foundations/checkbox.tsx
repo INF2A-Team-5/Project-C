207e114ea74d0995c4f3eb46d1d57baa52c9ffb2
fpsven <95655446+fpsven@users.noreply.github.com>
2023-11-23 21:58:58 +0100

    checkbox design

--- frontend/src/components/foundations/checkbox.tsx
+++ frontend/src/components/foundations/checkbox.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import {
+    CheckIcon
+  } from '@radix-ui/react-icons';
 
 interface CheckboxProps extends React.HTMLAttributes<HTMLInputElement>{
     checked: boolean;
@@ -8,8 +11,16 @@ interface CheckboxProps extends React.HTMLAttributes<HTMLInputElement>{
 
 function Checkbox({ checked, onChange }: CheckboxProps): JSX.Element {
     return (
-        <div className="items-top flex space-x-2">
-            <input type="checkbox" checked={checked} onChange={onChange} />
+        <div className="flex relative">
+            <input className='transparent z-10 mr-4 w-8 h-8 appearance-none border-2 border-primary-500 rounded-xl'
+            id='checkbox' type="checkbox" checked={checked} onChange={onChange} 
+            placeholder=''/>
+            
+            <div className=''>
+         
+                <CheckIcon style={{backgroundColor:"transparent"}} color='hsl(221.2 83.2% 50%)' 
+                className='check-1 opacity-0 absolute top-2 left-2 scale-[2]'/>
+            </div>
         </div>
     );
 }
