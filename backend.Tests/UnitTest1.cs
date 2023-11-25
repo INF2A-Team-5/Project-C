2654af6902913e2006f7db64c5ac98ed484e773f
fpsven <sven.groeneveld.nl@gmail.com>
2023-11-25 12:16:48 +0100

    tests setup(kinda?)

---
+++ backend.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace backend.Tests
+{
+    public class dummyTests
+    {
+        [Fact]
+        public static void dummy_dummyString_ReturnsString()
+        {
+
+        }
+    }
+}
