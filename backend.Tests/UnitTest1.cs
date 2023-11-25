6e1215b83e477705e95bd566185dc8df41d6a71c
fpsven <sven.groeneveld.nl@gmail.com>
2023-11-25 13:24:14 +0100

    .

--- backend.Tests/UnitTest1.cs
+++ backend.Tests/UnitTest1.cs
@@ -1,13 +1,44 @@
 using Xunit;
+using backend.Services;
+﻿using FakeItEasy;
+
 
 namespace backend.Tests
 {
-    public class dummyTests
+    public class SetuptestsTest
     {
-        [Fact]
-        public static void dummy_dummyString_ReturnsString()
+        private Setuptests _setuptests;
+        public SetuptestsTest()
         {
-
+            _setuptests = new Setuptests(0);
+        }
+        [Fact]
+        public void Setuptests_gkhjfsd_ReturnsString()
+        {
+            // Arrange
+            bool input = true;
+            var expected = "true";
+            // Act
+            var result = _setuptests.gkhjfsd(input);
+            // Assert
+            Assert.Equal(result, expected);
+        }
+    }
+    public class SetuptestssTest
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            //Arrange
+            var setuptestss = new Setuptestss();
+            var expected = "true";
+            //Act
+            var result = setuptestss.gkhjfsdd(value);
+            //Assert
+            Assert.Equal(result, expected);
         }
     }
 }
--- backend/Services/Setuptests.cs
+++ backend/Services/Setuptests.cs
@@ -7,6 +7,36 @@ namespace backend.Services
 {
     public class Setuptests
     {
+        private readonly int _i; 
+        public Setuptests(int i)
+        {
+            _i = i;
+        }
+        public string gkhjfsd(bool r = false)
+        {
+            if (r)
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }
+        }
+    }
+    public class Setuptestss
+    {
+        public string gkhjfsdd(int r)
+        {
+            if (r > 100)
+            {
+                return "false";
+            }
+            else
+            {
+                return "true";
+            }
+        }
         
     }
 }
