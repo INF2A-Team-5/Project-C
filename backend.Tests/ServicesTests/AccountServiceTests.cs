3ed206c80c850814e31533de4a01f6b59dbc3f2c
fpsven <sven.groeneveld.nl@gmail.com>
2023-11-26 16:44:52 +0100

    first test services

--- backend.Tests/ServicesTests/AccountServiceTests.cs
+++ backend.Tests/ServicesTests/AccountServiceTests.cs
@@ -1,26 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Backend.AccountService;
 using Xunit;
 using FakeItEasy;
-using Backend.Data;
 using Backend.Entities;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 namespace backend.Tests.ServicesTests
 {
     public class AccountServiceTests
     {
+        private readonly IAccountService _fakeAccountService;
+        public AccountServiceTests()
+        {
+            _fakeAccountService = A.Fake<IAccountService>();
+        }
+
         [Fact]
         public async void AccountService_GetAllAccounts_ReturnsAllAccounts()
         {
             // Arrange
-            var fakeAccountService = A.Fake<IAccountService>();
-            A.CallTo(() => fakeAccountService.GetAllAccounts()).Returns(new List<Account>());
+            A.CallTo(() => _fakeAccountService.GetAllAccounts()).Returns(new List<Account>());
             // Act
-            var result = await fakeAccountService.GetAllAccounts();
+            var result = await _fakeAccountService.GetAllAccounts();
             // Assert
             Assert.NotNull(result);
             Assert.IsType<List<Account>>(result.Value);
@@ -33,10 +32,9 @@ namespace backend.Tests.ServicesTests
         public async void AccountService_GetAccountById_ReturnsAccount(int id)
         {
             // Arrange
-            var fakeAccountService = A.Fake<IAccountService>();
-            A.CallTo(() => fakeAccountService.GetAccountById(id)).Returns(new Account());
+            A.CallTo(() => _fakeAccountService.GetAccountById(id)).Returns(new Account());
             // Act
-            var result = await fakeAccountService.GetAccountById(id);
+            var result = await _fakeAccountService.GetAccountById(id);
             // Assert
             Assert.NotNull(result);
             Assert.IsType<Account>(result.Value);
@@ -46,36 +44,46 @@ namespace backend.Tests.ServicesTests
         [InlineData(1, "clientname" , "newPw", AccountType.Client)]
         [InlineData(2, "empname" , "newPw", AccountType.ServiceEmployee)]
         [InlineData(3, "adminname" , "newPw", AccountType.Admin)]
-        public async void AccountService_UpdateAccount_ReturnsIActionResult(int id, string name, string password, AccountType accountType)
+        public async void AccountService_UpdateAccount_ReturnsNoContent(int id, string name, string password, AccountType accountType)
         {
             // Arrange
-            var fakeAccountService = A.Fake<IAccountService>();
             var newPw = "newPw";
-            var updatedAcc = new Account {AccountId = id, Name = name, Password = password, Class = accountType };
-            A.CallTo(() => fakeAccountService.UpdateAccount(id, updatedAcc)).Returns(new OkObjectResult(updatedAcc));
+            var fakeAccount = new Account {AccountId = id, Name = name, Password = password, Class = accountType };
+            A.CallTo(() => _fakeAccountService.UpdateAccount(id, fakeAccount)).Returns(new OkObjectResult(fakeAccount));
             // Act
-            var result = await fakeAccountService.UpdateAccount(id, new Account { Name = name, Password = newPw, Class = accountType });
+            var result = await _fakeAccountService.UpdateAccount(id, new Account { Name = name, Password = newPw, Class = accountType });
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(updatedAcc.Password, newPw);
+            Assert.Equal(fakeAccount.Password, newPw);
         }
         
         [Theory]
         [InlineData("testclient" , "testclientpw", AccountType.Client)]
         [InlineData("testemp" , "testemppw", AccountType.ServiceEmployee)]
         [InlineData("testadmin" , "testadminpw", AccountType.Admin)]
-        public async void AccountService_AddAccount_ReturnsIActionResult(string name, string password, AccountType accountType)
+        public async void AccountService_AddAccount_ReturnsCreatedAtActionResult(string name, string password, AccountType accountType)
         {
             // Arrange
-            var fakeAccountService = A.Fake<IAccountService>();
             var newAccount = new Account {Name = name, Password = password, Class = accountType };
-            A.CallTo(() => fakeAccountService.AddAccount(newAccount)).Returns(new Account());
+            A.CallTo(() => _fakeAccountService.AddAccount(newAccount)).Returns(new Account());
             // Act
-            var result = await fakeAccountService.AddAccount(new Account { Name = name, Password = password, Class = accountType });
+            var result = await _fakeAccountService.AddAccount(new Account { Name = name, Password = password, Class = accountType });
             // Assert
             Assert.NotNull(result);
-            Assert.IsType<NoContentResult>(result);
         }
         
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public async void AccountService_DeleteAccount_ReturnsIActionResult(int id)
+        {
+            // Arrange
+            A.CallTo(() => _fakeAccountService.DeleteAccount(id)).Returns(new OkObjectResult(id));
+            // Act
+            var result = await _fakeAccountService.DeleteAccount(id);
+            // Assert
+            Assert.NotNull(result);
+        }
     }
 }
