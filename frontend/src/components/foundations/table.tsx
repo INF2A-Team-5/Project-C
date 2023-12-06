61d2d7bf8102db659d7abc2d5f7f6c88c30d6622
fpsven <95655446+fpsven@users.noreply.github.com>
2023-12-06 22:23:18 +0100

    add styling table

--- frontend/src/components/foundations/table.tsx
+++ frontend/src/components/foundations/table.tsx
@@ -7,11 +7,19 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+} from "@/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Button } from "../ui/button";
 import { MoreHorizontal } from "lucide-react";
 import { Card } from "../ui/card";
+import { Separator } from "../ui/separator";
 
 interface TableProps {
   data: { [key: string]: any }[];
@@ -22,7 +30,7 @@ interface TableProps {
 function DataTable({ data, displayColumns, dataColumns }: TableProps) {
   const [currentPage, setCurrentPage] = useState(1);
   const [sortColumn, setSortColumn] = useState<string | null>(null);
-  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
 
   const handleSort = (column: string) => {
     let temp;
@@ -37,7 +45,7 @@ function DataTable({ data, displayColumns, dataColumns }: TableProps) {
         temp = "customer_Id";
         break;
       case "Date":
-        temp = "date_Created"
+        temp = "date_Created";
         break;
       case "Status":
         temp = "status";
@@ -48,17 +56,19 @@ function DataTable({ data, displayColumns, dataColumns }: TableProps) {
     }
 
     if (sortDirection == "asc") {
-      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
       sortByKey(data, temp);
     } else {
       setSortColumn(column);
-      setSortDirection('asc');
+      setSortDirection("asc");
       sortByKey(data, temp).reverse();
     }
   };
 
   function sortByKey<T>(array: T[], key: keyof T): T[] {
-    return array.sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
+    return array.sort((a, b) =>
+      a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0
+    );
   }
 
   const indexOfLastItem = currentPage * 10;
@@ -72,51 +82,50 @@ function DataTable({ data, displayColumns, dataColumns }: TableProps) {
   };
 
   async function handleButtonClick(ticket: any) {
-    const user = await fetch(`http://localhost:5119/api/Accounts/${localStorage.getItem("Id")}`, {
-      method: "GET",
-      headers:
+    const user = await fetch(
+      `http://localhost:5119/api/Accounts/${localStorage.getItem("Id")}`,
       {
-        "Content-Type": "application/json",
-        "Authorization": "bearer " + localStorage.getItem("Token"),
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "bearer " + localStorage.getItem("Token"),
+        },
       }
-    }).then((res) => res.json());
+    ).then((res) => res.json());
 
     if (user.class == "Admin" || user.class == "ServiceEmployee") {
       if (ticket.assigned_Id == null || ticket.assigned_Id == 0) {
-        const temp =
-        {
-          "TicketId": ticket.ticketId,
-          "Machine_Id": ticket.machine_Id,
-          "Customer_Id": ticket.customer_Id,
-          "Assigned_Id": localStorage.getItem("Id"),
-          "Priority": ticket.priority,
-          "Status": ticket.status,
+        const temp = {
+          TicketId: ticket.ticketId,
+          Machine_Id: ticket.machine_Id,
+          Customer_Id: ticket.customer_Id,
+          Assigned_Id: localStorage.getItem("Id"),
+          Priority: ticket.priority,
+          Status: ticket.status,
 
-          "Problem": ticket.problem,
-          "HaveTried": ticket.haveTried,
-          "MustBeDoing": ticket.mustBeDoing,
-          "Date_Created": ticket.date_Created,
+          Problem: ticket.problem,
+          HaveTried: ticket.haveTried,
+          MustBeDoing: ticket.mustBeDoing,
+          Date_Created: ticket.date_Created,
 
-          "Solution": ticket.solution,
-          "PhoneNumber": ticket.phoneNumber,
-          "Notes": ticket.notes,
-          "files": ticket.files
+          Solution: ticket.solution,
+          PhoneNumber: ticket.phoneNumber,
+          Notes: ticket.notes,
+          files: ticket.files,
         };
 
-        await fetch("http://localhost:5119/api/Tickets/" + temp.TicketId,
-          {
-            method: "PUT",
-            headers: {
-              "Authorization": "bearer " + localStorage.getItem("Token"),
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(temp)
-          });
+        await fetch("http://localhost:5119/api/Tickets/" + temp.TicketId, {
+          method: "PUT",
+          headers: {
+            Authorization: "bearer " + localStorage.getItem("Token"),
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(temp),
+        });
 
         localStorage.setItem("currentticket", ticket.ticketId.toString());
         window.location.href = "edit-ticket";
-      }
-      else {
+      } else {
         alert("Tickets is allready assigned");
       }
 
@@ -125,67 +134,88 @@ function DataTable({ data, displayColumns, dataColumns }: TableProps) {
   }
 
   return (
-    <Card>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            {displayColumns.map((column, index) => (
-              <TableHead key={index} onClick={() => { column == "" ? null : handleSort(column) }}>
-                {column} {sortColumn === column && (sortDirection === 'asc' ? <ArrowDownIcon /> : <ArrowUpIcon />)}
-              </TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {currentData.map((row, rowIndex) => (
-            <TableRow key={rowIndex}>
-              {dataColumns.map((column, columnIndex) => (
-                <TableCell key={columnIndex}>{row[column]}</TableCell>
+    <div>
+      <Card>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {displayColumns.map((column, index) => (
+                <TableHead
+                  key={index}
+                  onClick={() => {
+                    column == "" ? null : handleSort(column);
+                  }}
+                >
+                  <Button variant={"ghost"}>
+                    {column}{" "}
+                    {sortColumn === column &&
+                      (sortDirection === "asc" ? (
+                        <ArrowDownIcon />
+                      ) : (
+                        <ArrowUpIcon />
+                      ))}
+                  </Button>
+                </TableHead>
               ))}
-              <TableCell>
-                {/* <button onClick={() => handleButtonClick(row)}>Edit</button> */}
-                <div>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button variant="ghost" className="h-8 w-8 p-0">
-                        <span className="sr-only">Open menu</span>
-                        <MoreHorizontal className="h-4 w-4" />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                      <DropdownMenuItem
-                        onClick={() => alert(7)}
-                      >
-                        Show seven
-                      </DropdownMenuItem>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuItem>View customer</DropdownMenuItem>
-                      <DropdownMenuItem>View payment details</DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </div>
-              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-
-      <div>
-        <Button className="m-4" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
-          Previous
-        </Button>
-        <span>{`Page ${currentPage} of ${totalPages}`}</span>
-        <Button
-          className="m-4"
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          Next
-        </Button>
-      </div>
-    </Card>
-  )
+          </TableHeader>
+          <TableBody>
+            {currentData.map((row, rowIndex) => (
+              <TableRow key={rowIndex}                        >
+                {dataColumns.map((column, columnIndex) => (
+                  <TableCell className="pl-8" key={columnIndex}>{row[column]}</TableCell>
+                ))}
+                <TableCell>
+                  {/* <button onClick={() => handleButtonClick(row)}>Edit</button> */}
+                  <div>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                          <span className="sr-only">Open menu</span>
+                          <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem onClick={() => alert(7)}>
+                          Show seven
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+                        <DropdownMenuItem>
+                          View payment details
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Separator />
+        <div className="flex items-center justify-end">
+          <Button
+            className="m-4"
+            variant={"outline"}
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </Button>
+          <span>{`Page ${currentPage} of ${totalPages}`}</span>
+          <Button
+            className="m-4"
+            variant={"outline"}
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
 export default DataTable;
