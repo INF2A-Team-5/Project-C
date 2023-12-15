f5e986205b008621cd2320e73f5e41910c25a3df
Jeffrey <Jeffmanvan@gmail.com>
2023-12-15 15:17:23 +0100

    .

--- backend/entities/Customer.cs
+++ backend/entities/Customer.cs
@@ -6,6 +6,6 @@ namespace Backend.Entities;
         public int AccountId {get; set; } 
         public Account Account {get; set; } = null!;
         public ICollection<Machine> Machines { get; } = new List<Machine>();
-        public string? PhoneNumber {get; set; }
+        // public string? PhoneNumber {get; set; }
         public ICollection<Ticket> Tickets { get; } = new List<Ticket>();
     }
