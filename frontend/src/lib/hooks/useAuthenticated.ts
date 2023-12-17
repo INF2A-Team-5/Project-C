a97282fa95f92af1513d8eb95d614bae746e7eee
Jeffrey <Jeffmanvan@gmail.com>
2023-12-17 16:57:27 +0100

    .

--- frontend/src/lib/hooks/useAuthenticated.ts
+++ frontend/src/lib/hooks/useAuthenticated.ts
@@ -1,29 +1,24 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export function useAuthenticated() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
   const isLoggedIn = localStorage.getItem("Token") !== null;
-  
-  // function checkLogin() {
-  //   setIsLoggedIn(
-  //     (fetch(API_BASE_URL + "/api/accounts", getBaseQueryRequest())
-  //       .then((data) => data.json())
-  //       .then((accounts) =>
-  //         accounts.find(
-  //           (acc: any) =>
-  //             acc.name == localStorage.getItem("username") &&
-  //             acc.accountId.toString() == localStorage.getItem("Id") &&
-  //             acc.class == localStorage.getItem("Class")
-  //         ),
-  //       )) != undefined,
-  //   );
-  // }
+  const location = useLocation();
+
   useEffect(() => {
     // als users ingelogd is, set authenticated true
     if (isLoggedIn) {
       setIsAuthenticated(true);
+      if (
+        ((location.pathname == "/accounts" ||
+          location.pathname == "/departments") &&
+          localStorage.getItem("Class") == "ServiceEmployee") ||
+        localStorage.getItem("Class") == "Client"
+      ) {
+        navigate("/tickets");
+      }
     } else {
       navigate("/auth/login");
     }
