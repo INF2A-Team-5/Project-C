d8db45897dbd50bc27621d2618d455b516550398
ZoutigeWolf <guuskamphuis@gmail.com>
2024-01-15 16:12:05 +0100

    fix bugs

--- backend/models/AccountType.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-[JsonConverter(typeof(JsonStringEnumConverter))]
-public enum AccountType
-{
-    Admin = 1,
-    Client,
-    ServiceEmployee        
-}
--- backend/models/StatusType.cs
+++
@@ -1,6 +0,0 @@
-public enum StatusType
-{
-    Open,
-    InProcess,
-    Closed
-}
